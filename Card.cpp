3845ed1154f3865b9f9c1aa023b15de5a83a6bdc
Devon Havers <Devon66h@gmail.com>
2018-09-06 16:41:38 -0400

    Created card class that allows you to create a card with a give number. 1 = Ace, 2-10 = Normal, 11 = Jack, 12 = Queen, 13 = King

---
+++ Card.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Devon on 9/6/2018.
+//
+
+#include "Card.h"
+
+Card::Card(int cardNumber) {
+    num = cardNumber;
+}
---
+++ Card.h
@@ -0,0 +1,21 @@
+//
+// Created by Devon on 9/6/2018.
+//
+
+#ifndef CS205_GOFISH_CARD_H
+#define CS205_GOFISH_CARD_H
+
+#endif //CS205_GOFISH_CARD_H
+
+class Card {
+private:
+    int num;
+
+public:
+    /*
+     * Constructor that takes a card number as n
+     * and sets the integer num variable
+     * 1 = Ace, 2-10 = Normal, 11 = Jack, 12 = Queen, 13 = King
+     */
+    Card(int n);
+};
