e629ae68b2ea5f983a816f42d938d93de52201ee
Griffin P. Georgiadis <griffin.georgiadis@uvm.edu>
2018-09-06 15:46:03 -0400

    first commit

---
+++ src/gofish205.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : gofish205.cpp
+// Author      : Griffin Georgiadis, Nick Hallowell, Devon Havers, Rob Duarte
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
